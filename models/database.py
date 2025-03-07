0cd53e9810d47abe1a03107ba0aa3960019b570b
jeancgarciaq <jcvictory@hotmail.com>
2025-03-07 00:08:26 -0400

    Modelo principal para la conexion a la bd

--- models/database.py
+++ models/database.py
@@ -1,1 +1,34 @@
-# Configuración de la base de datos
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# Ruta de la base de datos
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE_PATH = os.path.join(BASE_DIR, 'app.db')
+DATABASE_URL = f'sqlite:///{DATABASE_PATH}'
+
+# Crea el motor de la base de datos
+engine = create_engine(DATABASE_URL)
+
+# Crea una sesión para interactuar con la base de datos
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Crea una clase base declarativa para los modelos
+Base = declarative_base()
+
+# Función para crear la base de datos si no existe
+def create_database():
+    if not os.path.exists(DATABASE_PATH):
+        Base.metadata.create_all(bind=engine)
+
+# Llama a la función para crear la base de datos
+create_database()
+
+# Función para obtener una sesión de la base de datos
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
