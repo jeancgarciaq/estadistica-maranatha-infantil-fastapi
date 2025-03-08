9582c28c630ef5bd9125b31924179bc387904af5
jeancgarciaq <jcvictory@hotmail.com>
2025-03-08 00:43:37 -0400

    Controlador, vista y modelo de areas listo para prueba

--- controllers/areas_controller.py
+++ controllers/areas_controller.py
@@ -1,1 +1,41 @@
-# Lógica de Áreas
+from models.areas import Area
+from models.database import get_db
+from sqlalchemy.orm import Session
+
+class ControladorArea:
+    def __init__(self, vista):
+        self.vista = vista
+
+    def crear_area(self, nombre):
+        db: Session = next(get_db())
+        area = Area(nombre=nombre)
+        db.add(area)
+        db.commit()
+        db.refresh(area) # Actualiza el objeto area con los datos de la base de datos
+        self.listar_areas()
+
+    def actualizar_area(self, id, nombre):
+        db: Session = next(get_db())
+        area = db.query(Area).filter(Area.id == id).first()
+        if area:
+            area.nombre = nombre
+            db.commit()
+            self.listar_areas()
+
+    def eliminar_area(self, id):
+        db: Session = next(get_db())
+        area = db.query(Area).filter(Area.id == id).first()
+        if area:
+            db.delete(area)
+            db.commit()
+            self.listar_areas()
+
+    def listar_areas(self):
+        db: Session = next(get_db())
+        areas = db.query(Area).all()
+        self.vista.actualizar_lista_areas(areas)
+
+    def obtener_area(self, id):
+        db: Session = next(get_db())
+        area = db.query(Area).filter(Area.id == id).first()
+        return area
--- models/areas.py
+++ models/areas.py
@@ -1,1 +1,8 @@
-# Modelo de Áreas
+from sqlalchemy import Column, Integer, String
+from models.database import Base
+
+class Area(Base):
+    __tablename__ = 'areas'
+
+    id = Column(Integer, primary_key=True, index=True)
+    area = Column(String)
