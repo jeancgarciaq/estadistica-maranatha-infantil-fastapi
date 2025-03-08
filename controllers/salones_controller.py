256c10eeff07e9cdb1fd110844971ed069e342b5
jeancgarciaq <jcvictory@hotmail.com>
2025-03-08 00:44:12 -0400

    Controlador, modelo y vista de salones listo para pruebas

--- controllers/salones_controller.py
+++ controllers/salones_controller.py
@@ -1,1 +1,41 @@
-# Lógica de Salones
+from models.salon import Salon
+from models.database import get_db
+from sqlalchemy.orm import Session
+
+class ControladorSalon:
+    def __init__(self, vista):
+        self.vista = vista
+
+    def crear_salon(self, salon, edad):
+        db: Session = next(get_db())
+        nuevo_salon = Salon(salon=salon, edad=edad)
+        db.add(nuevo_salon)
+        db.commit()
+        db.refresh(nuevo_salon)
+        self.listar_salones()
+
+    def actualizar_salon(self, id, salon, edad):
+        db: Session = next(get_db())
+        salon_actualizar = db.query(Salon).filter(Salon.id == id).first()
+        if salon_actualizar:
+            salon_actualizar.salon = salon
+            salon_actualizar.edad = edad
+            db.commit()
+            self.listar_salones()
+
+    def eliminar_salon(self, id):
+        db: Session = next(get_db())
+        salon_eliminar = db.query(Salon).filter(Salon.id == id).first()
+        if salon_eliminar:
+            db.delete(salon_eliminar)
+            db.commit()
+            self.listar_salones()
+
+    def listar_salones(self):
+        db: Session = next(get_db())
+        salones = db.query(Salon).all()
+        self.vista.actualizar_lista_salones(salones)
+
+    def obtener_salon(self, id):
+        db: Session = next(get_db())
+        return db.query(Salon).filter(Salon.id == id).first()
--- models/salones.py
+++ models/salones.py
@@ -1,1 +1,9 @@
-# Modelo de Salones
+from sqlalchemy import Column, Integer, String
+from models.database import Base
+
+class Salon(Base):
+    __tablename__ = 'salones'
+
+    id = Column(Integer, primary_key=True, index=True)
+    salon = Column(String)
+    edad = Column(String)
