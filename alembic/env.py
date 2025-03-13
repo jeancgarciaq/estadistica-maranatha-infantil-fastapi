3be03764ea90912b04d3d89b14167f7c46093081
jeancgarciaq <jcvictory@hotmail.com>
2025-03-13 00:08:49 -0400

    Creación de la primera migración de alembic y base de datos

--- alembic/env.py
+++ alembic/env.py
@@ -10,9 +10,9 @@ import sys
 from logging.config import fileConfig
 
 #agregado
-sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), 'models'))
 from models.database import Base
-target_metadata = Base.metadata
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -27,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -74,7 +74,7 @@ def run_migrations_online():
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata, **current_app
+            connection=connection, target_metadata=target_metadata
         )
 
         with context.begin_transaction():
