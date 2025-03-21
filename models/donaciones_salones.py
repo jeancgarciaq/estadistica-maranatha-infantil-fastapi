c5d4379e48407b09116674f2ba75b6c165bbe72f
jeancgarciaq <jcvictory@hotmail.com>
2025-03-21 00:34:11 -0400

    Eliminacion modelo donaciones_salones

--- models/donaciones_salones.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Table, Column, Integer, ForeignKey
-from models.database import Base
-
-donaciones_salones = Table(
-    'donaciones_salones', Base.metadata,
-    Column('donacion_id', Integer, ForeignKey('donaciones.id'), primary_key=True),
-    Column('salon_id', Integer, ForeignKey('salones.id'), primary_key=True)
-)
