e3305cabd40162f4d06fa5fc7d5a7f21a6071988
jeancgarciaq <jcvictory@hotmail.com>
2025-03-23 01:28:01 -0400

    feat: corrección importación archivo KV

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from kivy.lang import Builder
 from screens import MenuScreen, AreasScreen, SalonesScreen, EstadisticaScreen, DonacionesScreen, DistribucionesScreen, LogisticaScreen, OtrasAreasScreen, EnsenanzaScreen, RecepcionScreen, ReporteScreen, AyudaScreen
 
 # Cargar archivos KV
-Builder.load_file('styles/app.kv')
-Builder.load_file('main.kv')
+Builder.load_file('views/menu.kv')
 
 # Definir el ScreenManager
 class WindowManager(ScreenManager):
