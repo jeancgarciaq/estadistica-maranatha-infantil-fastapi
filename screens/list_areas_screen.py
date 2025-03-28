c69c5bd5bc249600be9355923e102e5d1e1e0341
jeancgarciaq <jcvictory@hotmail.com>
2025-03-28 01:11:48 -0400

    feat: modifique funcionalidad de la clase

--- screens/list_areas_screen.py
+++ screens/list_areas_screen.py
@@ -1,6 +1,10 @@
 import logging
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.button import Button
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -8,8 +12,42 @@ logger = logging.getLogger(__name__)
 
 class ListAreasScreen(Screen):
     """Screen for displaying the list of areas."""
-
-    def __init__(self, **kwargs):
+        
+    def __init__(self, controlador, **kwargs):
         super().__init__(**kwargs)
         logger.info("Initializing ListAreasScreen")
-        Builder.load_file("list_areas.kv")
+        Builder.load_file("views/list_areas.kv")
+        self.controlador = controlador
+        self.layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+
+        self.title = Label(text="Lista de Áreas", size_hint=(1, 0.1), font_size='20sp', bold=True)
+        self.layout.add_widget(self.title)
+
+        # Contenedor para la lista de áreas
+        self.scrollview = ScrollView(size_hint=(1, 0.8))
+        self.areas_container = BoxLayout(orientation='vertical', size_hint_y=None)
+        self.areas_container.bind(minimum_height=self.areas_container.setter('height'))
+        self.scrollview.add_widget(self.areas_container)
+        self.layout.add_widget(self.scrollview)
+
+        # Botón para regresar
+        self.btn_back = Button(text="Volver", size_hint=(1, 0.1))
+        self.btn_back.bind(on_press=self.volver)
+        self.layout.add_widget(self.btn_back)
+
+        self.add_widget(self.layout)
+
+    def actualizar_lista_areas(self, areas):
+        """Actualiza la lista de áreas en la vista"""
+        self.areas_container.clear_widgets()
+
+        if not areas:
+            self.areas_container.add_widget(Label(text="No hay áreas registradas", font_size='18sp'))
+        else:
+            for area in areas:
+                self.areas_container.add_widget(Label(text=f"{area.id}: {area.area}", font_size='16sp'))
+
+    def volver(self, instance):
+        """Regresa a la pantalla de áreas"""
+        self.manager.current = 'areas'
+
