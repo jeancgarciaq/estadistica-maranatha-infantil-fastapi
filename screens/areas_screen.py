32721e4abf474e4e8715d061622d86187f685442
jeancgarciaq <jcvictory@hotmail.com>
2025-03-28 01:23:39 -0400

    feat: borrar método mostrar_popup_lista

--- screens/areas_screen.py
+++ screens/areas_screen.py
@@ -54,37 +54,6 @@ class AreasScreen(Screen):
             self.ids.area_nombre.text = area.nombre
             self.ids.area_id.text = str(area.id)
 
-    def mostrar_popup_lista(self):
-        areas = self.controlador.obtener_todas_las_areas()  # Nueva función en el controlador que retorna la lista de áreas
-
-        # Crear el layout para la lista
-        lista_areas_popup = GridLayout(cols=2, size_hint_y=None)
-        lista_areas_popup.bind(minimum_height=lista_areas_popup.setter('height'))
-
-        for area in areas:
-            lista_areas_popup.add_widget(Label(text=str(area.id)))
-            lista_areas_popup.add_widget(Label(text=area.nombre))
-
-        # Crear el ScrollView para la lista
-        scrollview = ScrollView(size_hint=(1, 1))
-        scrollview.add_widget(lista_areas_popup)
-
-        # Crear el botón de cerrar
-        close_button = Button(text='Cerrar', size_hint_y=None, height=50)
-
-        # Crear el popup
-        popup_content = BoxLayout(orientation='vertical')
-        popup_content.add_widget(scrollview)
-        popup_content.add_widget(close_button)
-
-        popup = Popup(title='Lista de Áreas', content=popup_content, size_hint=(None, None), size=(400, 400))
-
-        # Asignar la función de cierre al botón
-        close_button.bind(on_press=popup.dismiss)
-
-        # Mostrar el popup
-        popup.open()
-
     def mostrar_error(self, mensaje):
         popup = Popup(title='Error', content=Label(text=mensaje), size_hint=(None, None), size=(400, 200))
         popup.open()
