fc1a17cc2b92fe62267335a438d882bb706e7462
jeancgarciaq <jcvictory@hotmail.com>
2025-03-30 12:42:22 -0400

    feat: agregue código para debuggear

--- screens/list_salones_screen.py
+++ screens/list_salones_screen.py
@@ -3,18 +3,30 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.lang import Builder
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class ListSalonesScreen(Screen):
+    """ Pantalla que muestra una lista de salones. """
     def __init__(self, controlador, **kwargs):
-        Builder.load_file('views/list_salones.kv')
+        try:
+            Builder.load_file('views/list_salones.kv')
+        except Exception as e:
+            logger.error(f"Error al cargar list_salones.kv: {e}")
         super().__init__(**kwargs)
+        # Crear el controlador como atributo
         self.controlador = controlador
+        logger.info("Initializing ListSalonesScreen")
         self.cargar_salones()
 
     def on_pre_enter(self):
         self.cargar_salones()
 
     def cargar_salones(self):
+        """Actualiza la lista de salones en la vista"""
         lista_salones_grid = self.ids.lista_salones
         lista_salones_grid.clear_widgets()
         salones = self.controlador.obtener_todos_los_salones()
