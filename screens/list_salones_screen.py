71c80b9753a7f133356c44c464842a12f07a708f
jeancgarciaq <jcvictory@hotmail.com>
2025-03-30 10:42:52 -0400

    add: clase ListSalonesScreen

---
+++ screens/list_salones_screen.py
@@ -0,0 +1,19 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.lang import Builder
+
+class ListSalonesScreen(Screen):
+    def __init__(self, controlador, **kwargs):
+        Builder.load_file('views/list_salones.kv')
+        super().__init__(**kwargs)
+        self.controlador = controlador
+        self.cargar_salones()
+
+    def cargar_salones(self):
+        lista_salones_grid = self.ids.lista_salones
+        lista_salones_grid.clear_widgets()
+        salones = self.controlador.obtener_todos_los_salones()
+        for salon in salones:
+            lista_salones_grid.add_widget(Label(text=f"{salon.salon} ({salon.edad})", size_hint_y=None, height=40))
