df65b7fbeb803584f61361cefca2de23b88ee100
jeancgarciaq <jcvictory@hotmail.com>
2025-04-14 00:27:11 -0400

    feat: Se agrego docstring y validación de los datos

--- models/donaciones.py
+++ models/donaciones.py
@@ -3,14 +3,25 @@ from sqlalchemy.orm import relationship
 from models.base import Base
 
 class Donacion(Base):
+    """
+    Modelo que representa una donación en la base de datos.
+
+    Atributos:
+        id (int): Identificador único de la donación.
+        descripcion (str): Descripción de la donación.
+        cantidad (float): Cantidad de la donación.
+        unidad (str): Unidad de medida de la donación.
+        equipo (str): Equipo asociado con la donación.
+        fecha (date): Fecha en la que se realizó la donación.
+    """
     __tablename__ = 'donaciones'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    descripcion = Column(String)
-    cantidad = Column(Float)
-    unidad = Column(String)
-    equipo = Column(String)
-    fecha = Column(Date)
+    descripcion = Column(String(255), nullable=False)  # Longitud máxima de 255 caracteres
+    cantidad = Column(Float, nullable=False)  # No permite valores nulos
+    unidad = Column(String(50), nullable=False)  # Longitud máxima de 50 caracteres
+    equipo = Column(String(100))  # Longitud máxima de 100 caracteres, permite nulos
+    fecha = Column(Date, nullable=False)  # No permite valores nulos
 
     distribuciones = relationship("Distribucion", back_populates="donaciones")
-        
+
