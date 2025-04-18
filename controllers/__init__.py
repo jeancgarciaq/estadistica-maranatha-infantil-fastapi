311d0690bfb2285414ff1e291d41df6637b57e4f
jeancgarciaq <jcvictory@hotmail.com>
2025-04-17 23:27:14 -0400

    feat: corregí la importación de BaseController

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,4 +1,4 @@
-from base_controller import BaseController
+from .base_controller import BaseController
 from .areas_controller import AreasController
 from .aulas_controller import AulasController
 from .donaciones_controller import DonacionesController
