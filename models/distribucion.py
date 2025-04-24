c86d704f35fec42b95b9a5cb28f878246a489df9
jeancgarciaq <jcvictory@hotmail.com>
2025-04-23 23:18:29 -0400

    feat: Agregue validación, campo fecha y __repr__

--- models/distribucion.py
+++ models/distribucion.py
@@ -1,15 +1,22 @@
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from models.base import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 class Distribucion(Base):
     __tablename__ = 'distribuciones'
 
-    id = Column(Integer, primary_key=True)
-    donacion_id = Column(Integer, ForeignKey('donaciones.id'))
-    salon_id = Column(Integer, ForeignKey('salones.id'))
-    cantidad = Column(Float)
-    unidad = Column(String)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    donacion_id = Column(Integer, ForeignKey('donaciones.id'), nullable=False)
+    salon_id = Column(Integer, ForeignKey('salones.id'), nullable=False)
+    cantidad = Column(Float, nullable=False)
+    unidad = Column(String(50), nullable=False)
+    fecha = Column(Date, nullable=False)
 
-    donaciones = relationship("Donacion", back_populates="distribuciones")
-    salones = relationship("Salon", back_populates="distribuciones")
+    # Relaciones
+    donacion = relationship("Donacion", back_populates="distribuciones")
+    salon = relationship("Salon", back_populates="distribuciones")
+
+    def __repr__(self):
+        return f"<Distribucion(id={self.id}, donacion_id={self.donacion_id}, salon_id={self.salon_id}, cantidad={self.cantidad}, fecha='{self.fecha}')>"
