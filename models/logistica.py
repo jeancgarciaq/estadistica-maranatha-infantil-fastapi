6c3404e3e6571ff5cadcc3480f6a3a06daefee38
Jean Carlo García <jeansiervodedios@gmail.com>
2026-03-17 20:22:19 -0400

    Add __repr__ to Logistica model
    Introduce a __repr__ method in models/logistica.py to provide a clear, f-string-based string representation of Logistica instances (includes id, almacen, capitan, distribucion, hidratacion, pasillo, secretaria, fecha). This improves debugging and logging by making model instances easier to inspect.

--- models/logistica.py
+++ models/logistica.py
@@ -12,3 +12,6 @@ class Logistica(Base):
     pasillo = Column(Integer)
     secretaria = Column(Integer)
     fecha = Column(Date)
+
+    def __repr__(self):
+        return f"Logistica(id={self.id}, almacen={self.almacen}, capitan={self.capitan}, distribucion={self.distribucion}, hidratacion={self.hidratacion}, pasillo={self.pasillo}, secretaria={self.secretaria}, fecha='{self.fecha}')"
