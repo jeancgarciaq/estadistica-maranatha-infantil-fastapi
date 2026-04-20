72d99d6a6d5c7687ba0d20cfbbef28e689f13a73
Jean Carlo García <jeansiervodedios@gmail.com>
2026-04-19 22:21:53 -0400

    Export ListOtrasAreasScreen in screens package
    Add an import for ListOtrasAreasScreen in screens/__init__.py so the screen is exposed at the package level and can be imported directly (e.g. from screens import ListOtrasAreasScreen).

--- screens/__init__.py
+++ screens/__init__.py
@@ -16,3 +16,4 @@ from .list_salones_screen import ListSalonesScreen
 from .list_aulas_screen import ListAulasScreen
 from .list_donaciones_screen import ListDonacionesScreen
 from .list_distribuciones_screen import ListDistribucionesScreen
+from .list_otras_areas_screen import ListOtrasAreasScreen
