3cc73260588e27d1360b95ca73a3207a05898cc3
Jean Carlo García <jeansiervodedios@gmail.com>
2026-04-21 19:57:53 -0400

    Export ListPreparadosScreen from screens package
    Add import for ListPreparadosScreen to screens/__init__.py so the screen is re-exported at the package level and can be imported with 'from screens import ListPreparadosScreen'. This exposes the new list_preparados_screen module through the screens namespace.

--- screens/__init__.py
+++ screens/__init__.py
@@ -16,5 +16,6 @@ from .list_areas_screen import ListAreasScreen
 from .list_salones_screen import ListSalonesScreen
 from .list_aulas_screen import ListAulasScreen
 from .list_donaciones_screen import ListDonacionesScreen
+from .list_preparados_screen import ListPreparadosScreen
 from .list_distribuciones_screen import ListDistribucionesScreen
 from .list_otras_areas_screen import ListOtrasAreasScreen
