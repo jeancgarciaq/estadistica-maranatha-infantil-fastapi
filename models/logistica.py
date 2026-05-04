af187504ba01ccd975d3d5eee70869076ac555a3
Jean Carlo García <jeansiervodedios@gmail.com>
2026-05-03 23:31:02 -0400

    feat: add Logistica model and web templates for managing personnel logistics data

--- models/logistica.py
+++ models/logistica.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Date
+from sqlalchemy import Column, String, Date, Integer
 
 from models.database import Base
 from models.base_class import AuditMixin
@@ -6,12 +6,12 @@ from models.base_class import AuditMixin
 class Logistica(Base, AuditMixin):
     __tablename__ = 'logisticas'
 
-    almacen = Column(String(100), nullable=False)
-    capitan = Column(String(100), nullable=False)
-    distribucion = Column(String(255), nullable=True)
-    hidratacion = Column(String(255), nullable=True)
-    pasillo = Column(String(255), nullable=True)
-    secretaria = Column(String(255), nullable=True)
+    almacen = Column(Integer, nullable=False, default=0)
+    capitan = Column(Integer, nullable=False, default=0)
+    distribucion = Column(Integer, nullable=False, default=0)
+    hidratacion = Column(Integer, nullable=False, default=0)
+    pasillo = Column(Integer, nullable=False, default=0)
+    secretaria = Column(Integer, nullable=False, default=0)
     fecha = Column(Date, nullable=False)
 
     def __repr__(self):
