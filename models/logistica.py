05e1797b4751d11d5cd232c0ce13d1549d548804
Jean Carlo García <jeansiervodedios@gmail.com>
2026-05-18 21:51:14 -0400

    feat: add Logistica model for tracking event logistics and observations

--- models/logistica.py
+++ models/logistica.py
@@ -8,18 +8,8 @@ class Logistica(Base, AuditMixin):
     __tablename__ = 'logisticas'
 
     fecha = Column(Date, nullable=False, index=True)
-    id_capitan = Column(Integer, ForeignKey('servidores.id'), nullable=True)
     observaciones = Column(Text, nullable=True)
-
-    # Campos de resumen (se calculan para compatibilidad con reportes)
-    almacen = Column(Integer, default=0)
-    distribucion = Column(Integer, default=0)
-    hidratacion = Column(Integer, default=0)
-    pasillo = Column(Integer, default=0)
-    secretaria = Column(Integer, default=0)
-    capitan = Column(Integer, default=0) # Conteo para reportes
-
-    capitan_encargado = relationship("Servidor", foreign_keys=[id_capitan])
+    # Nota: Los capitanes y servidores ahora se consultan vía AsistenciaServidor
 
     def __repr__(self):
         return f"<Logistica(id={self.id}, fecha='{self.fecha}')>"
