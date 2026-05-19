86d1d0e7ec7040697fcdb906fb8aadb8a96f10f1
Jean Carlo García <jeansiervodedios@gmail.com>
2026-05-18 20:11:28 -0400

    feat: implement CRUD architecture and UI templates for management of servers, teachers, and collaborators

--- controllers/colaboradores_controller.py
+++ controllers/colaboradores_controller.py
@@ -1,5 +1,8 @@
 from controllers.base_controller import BaseController
 from models.colaboradores import Colaborador
+from models.capitanes import Capitan
+from models.coordinadores import Coordinador
+from sqlalchemy.orm import selectinload
 
 class ColaboradoresController(BaseController):
     def __init__(self, session=None):
@@ -8,7 +11,7 @@ class ColaboradoresController(BaseController):
     def listar_colaboradores(self):
         db = self.session if self.session else self.get_db_session()
         try:
-            query = self.query_activa(db)
+            query = self.query_activa(db).options(selectinload(Colaborador.capitan).selectinload(Capitan.coordinador).selectinload(Coordinador.area))
             return query.all()
         finally:
             if not self.session:
@@ -16,6 +19,8 @@ class ColaboradoresController(BaseController):
 
     def crear_colaborador(self, datos, user_context=None):
         def operacion(db):
+            # El área es heredada a través de la jerarquía
+            datos.pop('id_area', None)
             nuevo = Colaborador(**datos)
             db.add(nuevo)
             return nuevo
